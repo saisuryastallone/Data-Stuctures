bc625db6c9802dc476925a3823c02ef789f6a6fe
Sai Surya Pavuluri <65032235+saisuryastallone@users.noreply.github.com>
2021-06-05 00:38:22 +0100

    Add files via upload

---
+++ Binary_Tree.java
@@ -0,0 +1,83 @@
+
+public class Binary_Tree {
+	
+	public Node rootNode;
+	
+	// Let's create a Node class to represent nodes for the rest of the program
+	class Node {
+		public int value;
+		public Node leftChild;
+		public Node rightChild;
+	}
+	
+	// Let's initialize the root node upon the constructor call early on
+	Binary_Tree(int data){
+		rootNode = new Node();
+		rootNode.value = data;
+		rootNode.leftChild = null;
+		rootNode.rightChild = null;
+		
+	}
+	
+	// Let's insert child nodes from here
+	public boolean insert(int item) {
+		
+		Node parent = rootNode;
+		Node child = rootNode;
+		
+		Node n = new Node() ;
+		n.value = item;
+		n.leftChild = null;
+		n.rightChild = null;
+		
+		
+		while(child!=null){
+			
+			parent = child;
+			if(n.value > child.value) {
+				
+				child = child.rightChild;
+				
+			}
+		
+			else {
+				
+				child = child.leftChild;
+			}
+			}
+		
+			if(parent.value > n.value) {
+				parent.leftChild = n;
+				System.out.println("Inserted node "+n.value+" to the left of the node "+parent.value);
+			}
+			else {
+				parent.rightChild = n;
+				System.out.println("Inserted node "+n.value+" to the right of the node "+parent.value);
+			}
+		
+		
+			return true;
+		
+	}
+	
+	// Now it's time to delete some nodes
+	public boolean delete(int item) {
+		
+		
+	
+		return true;
+	}
+	
+	// Finally, let's display our tree
+	public void showTree(Node n) {
+		
+		if(n!=null) {
+			
+			showTree(n.leftChild);
+			System.out.println(n.value);
+			showTree(n.rightChild);
+			 }
+		}
+		
+	}
+	
